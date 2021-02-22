3d24bd477e8b9cb418cf3e9b9e4c2e7850577445
edvaudin <edward@vaudin.net>
2021-02-22 20:29:49 +0000

    Formatted Calculator Component

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -5,15 +5,15 @@ const Calculator = (props: any) => {
     //var storedUseClass = localStorage.getItem('useClass');]
     var longSpaces;
     var shortSpaces;
-    
-    switch(props.useClass.useClass){
+
+    switch (props.useClass.useClass) {
         case "A1 - Food retail": CalculateA1FoodSpaces();
             break;
         case "A1 - Non-food retail": CalculateA1NonFoodSpaces();
             break;
-        case "A2-A5 - Financial / professional services; cafes & restaurants; drinking establishments; take-aways" : CalculateA2toA5Spaces();
+        case "A2-A5 - Financial / professional services; cafes & restaurants; drinking establishments; take-aways": CalculateA2toA5Spaces();
             break;
-        case "B1 - Business offices" : CalculateB1OfficeSpaces();
+        case "B1 - Business offices": CalculateB1OfficeSpaces();
             break;
         case "B1 - Light industry and research and development": CalculateB1LightIndustrySpaces();
             break;
@@ -50,55 +50,55 @@ const Calculator = (props: any) => {
     }
 
     function CalculateA1FoodSpaces() {
-        if (props.useClass.requirements.GROSS_EXTERN_AREA >= 100){
-                longSpaces = Math.max((Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 175)), 2);
-            if (props.useClass.requirements.LOCATION != 0){
-                if (props.useClass.requirements.GROSS_EXTERN_AREA <= 750){
+        if (props.useClass.requirements.GROSS_EXTERN_AREA >= 100) {
+            longSpaces = Math.max((Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 175)), 2);
+            if (props.useClass.requirements.LOCATION != 0) {
+                if (props.useClass.requirements.GROSS_EXTERN_AREA <= 750) {
                     shortSpaces = Math.max((Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 20)), 2);
                 }
-                else{
+                else {
                     shortSpaces = 37.5 + Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 150);
                 }
             } else {
-                if (props.useClass.requirements.GROSS_EXTERN_AREA <= 750){
+                if (props.useClass.requirements.GROSS_EXTERN_AREA <= 750) {
                     shortSpaces = Math.max((Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 40)), 2);
                 }
-                else{
+                else {
                     shortSpaces = 18.75 + Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 300);
                 }
             }
         }
-        else{
+        else {
             longSpaces = 0;
             shortSpaces = 0;
         }
     }
 
-    function CalculateA1NonFoodSpaces(){
-        if (props.useClass.requirements.GROSS_EXTERN_AREA >= 100){
-            if (props.useClass.requirements.GROSS_EXTERN_AREA <= 1000){
+    function CalculateA1NonFoodSpaces() {
+        if (props.useClass.requirements.GROSS_EXTERN_AREA >= 100) {
+            if (props.useClass.requirements.GROSS_EXTERN_AREA <= 1000) {
                 longSpaces = Math.max((Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 250)), 2);
             }
-            else{
+            else {
                 longSpaces = 4 + Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 1000);
             }
-            if (props.useClass.requirements.LOCATION != 0){
-                if (props.useClass.requirements.GROSS_EXTERN_AREA <= 1000){
-                shortSpaces = Math.max((Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 60)), 2);
+            if (props.useClass.requirements.LOCATION != 0) {
+                if (props.useClass.requirements.GROSS_EXTERN_AREA <= 1000) {
+                    shortSpaces = Math.max((Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 60)), 2);
                 }
-                else{
-                shortSpaces = 16.67 + Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 500);
+                else {
+                    shortSpaces = 16.67 + Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 500);
                 }
             } else {
-                if (props.useClass.requirements.GROSS_EXTERN_AREA <= 1000){
-                shortSpaces = Math.max((Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 125)), 2);
+                if (props.useClass.requirements.GROSS_EXTERN_AREA <= 1000) {
+                    shortSpaces = Math.max((Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 125)), 2);
                 }
-                else{
-                shortSpaces = 8 + Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 1000);
+                else {
+                    shortSpaces = 8 + Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 1000);
                 }
             }
         }
-        else{
+        else {
             longSpaces = 0;
             shortSpaces = 0;
         }
@@ -128,7 +128,7 @@ const Calculator = (props: any) => {
         } else {
             Math.max((longSpaces = Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 150)), 2);
         }
-        
+
         if (props.useClass.requirements.GROSS_EXTERN_AREA <= 5000) {
             Math.max((shortSpaces = Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 500)), 2);
         }
@@ -137,93 +137,90 @@ const Calculator = (props: any) => {
         }
     }
 
-    function CalculateB1LightIndustrySpaces(){
+    function CalculateB1LightIndustrySpaces() {
         longSpaces = Math.max((Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 250)), 2);
         shortSpaces = Math.max((Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 1000)), 2);
     }
 
-    function CalculateB2toB8Spaces(){
+    function CalculateB2toB8Spaces() {
         longSpaces = Math.max((Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 500)), 2);
         shortSpaces = Math.max((Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 1000)), 2);
     }
 
-    function CalculateC1Spaces(){
+    function CalculateC1Spaces() {
         longSpaces = Math.max((Math.round(props.useClass.requirements.BEDROOMS / 20)), 2);
         shortSpaces = Math.max((Math.round(props.useClass.requirements.BEDROOMS / 50)), 2);
     }
 
-    function CalculateC2HospitalSpaces(){
+    function CalculateC2HospitalSpaces() {
         longSpaces = Math.max((Math.round(props.useClass.requirements.STAFF / 5)), 2);
         shortSpaces = Math.max((Math.round(props.useClass.requirements.STAFF / 30)), 2);
     }
 
-    function CalculateC2CareHomeSpaces(){
+    function CalculateC2CareHomeSpaces() {
         longSpaces = Math.max((Math.round(props.useClass.requirements.STAFF / 5)), 2);
         shortSpaces = Math.max((Math.round(props.useClass.requirements.BEDROOMS / 20)), 2);
     }
-    
-    function CalculateC3toC4Spaces(){
+
+    function CalculateC3toC4Spaces() {
         longSpaces = Math.max((Math.round(props.useClass.requirements.STUDIOS) + Math.round(props.useClass.requirements.TWO_PERSON_ONE_BEDROOM * 1.5) + (props.useClass.requirements.TWO_BEDROOM_PLUS * 2)), 2);
-        if (props.useClass.requirements.DWELLINGS < 5)
-        {
+        if (props.useClass.requirements.DWELLINGS < 5) {
             shortSpaces = 0;
         }
-        else if (props.useClass.requirements.DWELLINGS >= 5 && props.useClass.requirements.DWELLINGS <= 40)
-        {
+        else if (props.useClass.requirements.DWELLINGS >= 5 && props.useClass.requirements.DWELLINGS <= 40) {
             shortSpaces = 2;
         }
-        else
-        {
+        else {
             shortSpaces = 2 + Math.round(props.useClass.requirements.DWELLINGS / 40);
         }
     }
 
-    function CalculateD1NurserySpaces(){
+    function CalculateD1NurserySpaces() {
         longSpaces = Math.max((Math.round(props.useClass.requirements.STAFF / 8) + Math.round(props.useClass.requirements.STUDENTS / 8)), 2);
         shortSpaces = Math.max((Math.round(props.useClass.requirements.STAFF / 8) + Math.round(props.useClass.requirements.STUDENTS / 8)), 2);
     }
 
-    function CalculateD1SchoolSpaces(){
+    function CalculateD1SchoolSpaces() {
         longSpaces = Math.max((Math.round(props.useClass.requirements.STAFF / 8) + Math.round(props.useClass.requirements.STUDENTS / 8)), 2);
         shortSpaces = Math.max((Math.round(props.useClass.requirements.STUDENTS / 100)), 2);
     }
 
-    function CalculateD1UniSpaces(){
+    function CalculateD1UniSpaces() {
         longSpaces = Math.max((Math.round(props.useClass.requirements.STAFF / 4) + Math.round(props.useClass.requirements.STUDENTS / 20)), 2);
         shortSpaces = Math.max((Math.round(props.useClass.requirements.STUDENTS / 7)), 2);
     }
 
-    function CalculateD1HealthSpaces(){
+    function CalculateD1HealthSpaces() {
         longSpaces = Math.max((Math.round(props.useClass.requirements.STAFF / 5)), 2);
         shortSpaces = Math.max((Math.round(props.useClass.requirements.STAFF / 3)), 2);
     }
 
-    function CalculateD1OtherSpaces(){
+    function CalculateD1OtherSpaces() {
         longSpaces = Math.max((Math.round(props.useClass.requirements.STAFF / 8)), 2);
         shortSpaces = Math.max((Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 100)), 2);
     }
-    
-    function CalculateD2OtherSpaces(){
+
+    function CalculateD2OtherSpaces() {
         longSpaces = Math.max((Math.round(props.useClass.requirements.STAFF / 8)), 2);
         shortSpaces = Math.max((Math.round(props.useClass.requirements.SEATS / 30)), 2);
     }
 
-    function CalculateD2SportSpaces(){
+    function CalculateD2SportSpaces() {
         longSpaces = Math.max((Math.round(props.useClass.requirements.STAFF / 8)), 2);
         shortSpaces = Math.max((Math.round(props.useClass.requirements.GROSS_EXTERN_AREA / 100)), 2);
     }
 
-    function CalculateStudentAccommodationSpaces(){
+    function CalculateStudentAccommodationSpaces() {
         longSpaces = Math.max((Math.round(props.useClass.requirements.BEDROOMS * 0.75)), 2);
         shortSpaces = Math.max((Math.round(props.useClass.requirements.BEDROOMS / 40)), 2);
     }
 
-    function CalculateOlderPersonsSpaces(){
+    function CalculateOlderPersonsSpaces() {
         longSpaces = Math.max((Math.round(props.useClass.requirements.BEDROOMS / 10)), 2);
         shortSpaces = Math.max((Math.round(props.useClass.requirements.BEDROOMS / 40)), 2);
     }
 
-    return(
+    return (
         <div>
             <h2>Long-stay spaces: {longSpaces}</h2>
             <h2>Short-stay spaces: {shortSpaces}</h2>
